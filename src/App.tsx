59171ac95dd7fba935cf6df61bfd8a9d4182b57d
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-15 11:53:59 +0300

    test2 testing 1, 2 3,

--- src/App.tsx
+++ src/App.tsx
@@ -52,9 +52,9 @@ export default function App() {
         <br />
       </div>
 
-     
+      
       <div>
-        {json.map((obj) => (
+        {json.map((obj) => ( 
           <Card
             key={obj.id}
             artistName={obj.name}
