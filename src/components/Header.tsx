0eb682f440412c6e3d3c69d7a082d9d9ad25c11a
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-15 17:51:17 +0300

    tryin 2 clean up app.tsx with not much success, so rolling back n trying it different :D

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import HandleChange from "../App"
+import concatSearch from "../App"
+import lucky from "../App"
+export const header =
+
+(
+   <div>
+<h1 className="box">The Art Institute of Chicago</h1>
+
+<div className="center">
+  <input onChange={HandleChange} placeholder="Enter your search here" />
+</div>
+<div className="center">
+  <button onClick={concatSearch} type="button" className="btn btn-info">
+    Search
+  </button>
+  <button onClick={lucky} className="btn btn-info" type="button">
+    Randomize it !
+  </button>
+
+  <br /> 
+  </div>
+)
