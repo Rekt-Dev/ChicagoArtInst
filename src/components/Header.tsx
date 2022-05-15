7b7e85e1560a2cbd8581a067f7cf171039eedc7e
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-15 18:53:14 +0300

    header is perfect commiting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,24 @@ import HandleChange from "../App";
 import concatSearch from "../App";
 import lucky from "../App";
 
-export const header = (
-  <div>
-    <h1 className="box">The Art Institute of Chicago</h1>
+export function Header(type: any) {
+  return (
+    <div>
+      <h1 className="box">The Art Institute of Chicago</h1>
 
-    <div className="center">
-      <input onChange={HandleChange} placeholder="Enter your search here" />
-    </div>
-    <div className="center">
-      <button onClick={concatSearch} type="button" className="btn btn-info">
-        Search
-      </button>
-      <button onClick={lucky} className="btn btn-info" type="button">
-        Randomize it !
-      </button>
+      <div className="center">
+        <input onChange={HandleChange} placeholder="Enter your search here" />
+      </div>
+      <div className="center">
+        <button onClick={concatSearch} type="button" className="btn btn-info">
+          Search
+        </button>
+        <button onClick={lucky} className="btn btn-info" type="button">
+          Randomize it !
+        </button>
 
-      <br />
+        <br />
+      </div>
     </div>
-  </div>
-);
+  );
+}
