39d8ce1b84f06f816391533004b9810839bb460f
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-17 12:10:23 +0300

    added randomizer an outside component to help with randoing through 26 abc letters

---
+++ src/components/Randomizer.tsx
@@ -0,0 +1,137 @@
+export function numToLetter(num) {
+  let value1 = Math.floor(Math.random() * 26);
+  let r1 = (Math.random() * 10).toString(36).substring(9);
+
+  let a = "a";
+  let b = "b";
+  let c = "c";
+  let d = "d";
+  let e = "e";
+  let f = "f";
+  let g = "g";
+  let h = "h";
+  let i = "i";
+  let j = "j";
+  let k = "k";
+  let l = "l";
+  let m = "m";
+  let n = "n";
+  let o = "o";
+  let p = "p";
+  let q = "q";
+  let r = "r";
+  let s = "s";
+  let t = "t";
+  let u = "u";
+  let v = "v";
+  let w = "w";
+  let x = "x";
+  let y = "y";
+  let z = "z";
+
+  switch (num) {
+    case 0:
+      num = "a";
+      break;
+
+    case 1:
+      num = "b";
+      break;
+
+    case 2:
+      num = "c";
+      break;
+
+    case 3:
+      num = "d";
+      break;
+
+    case 4:
+      num = "e";
+      break;
+
+    case 5:
+      num = "f";
+      break;
+
+    case 6:
+      num = "g";
+      break;
+
+    case 7:
+      num = "h";
+      break;
+
+    case 8:
+      num = "i";
+      break;
+
+    case 9:
+      num = "j";
+      break;
+
+    case 10:
+      num = "k";
+      break;
+
+    case 11:
+      num = "l";
+      break;
+
+    case 12:
+      num = "m";
+      break;
+
+    case 13:
+      num = "n";
+      break;
+
+    case 14:
+      num = "o";
+      break;
+
+    case 15:
+      num = "p";
+      break;
+
+    case 16:
+      num = "q";
+      break;
+
+    case 17:
+      num = "r";
+      break;
+
+    case 18:
+      num = "s";
+      break;
+
+    case 19:
+      num = "t";
+      break;
+
+    case 20:
+      num = "u";
+      break;
+
+    case 21:
+      num = "v";
+      break;
+
+    case 22:
+      num = "w";
+      break;
+
+    case 23:
+      num = "x";
+      break;
+
+    case 24:
+      num = "y";
+      break;
+
+    case 25:
+      num = "z";
+      break;
+  }
+}
