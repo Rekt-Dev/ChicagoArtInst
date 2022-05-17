f23cfb255bbfcfaec2db74c2169dac980a58f34a
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-17 12:54:45 +0300

    more randomizing

--- src/components/Randomizer.tsx
+++ src/components/Randomizer.tsx
@@ -4,186 +4,280 @@ export function numToLetter(num: any) {
   console.log(`value1 ${value1}`);
   console.log(`r1 ${r1}`);
 
-  let a = "a";
-  let b = "b";
-  let c = "c";
-  let d = "d";
-  let e = "e";
-  let f = "f";
-  let g = "g";
-  let h = "h";
-  let i = "i";
-  let j = "j";
-  let k = "k";
-  let l = "l";
-  let m = "m";
-  let n = "n";
-  let o = "o";
-  let p = "p";
-  let q = "q";
-  let r = "r";
-  let s = "s";
-  let t = "t";
-  let u = "u";
-  let v = "v";
-  let w = "w";
-  let x = "x";
-  let y = "y";
-  let z = "z";
+  const link3 = "https://api.artic.edu/api/v1/artworks/search?limit=1q=";
+
+  const link4 = link3 + num;
+  let link5 = "";
+  console.log(`link4 ${link4}`);
+
+  const requestOptions3 = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: "randoing the db through the api" })
+  };
+
+  fetch(link5, requestOptions3)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
   switch (value1) {
     case 0:
       num = "a";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
       break;
 
     case 1:
       num = "b";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 2:
       num = "c";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 3:
       num = "d";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 4:
       num = "e";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 5:
       num = "f";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 6:
       num = "g";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 7:
       num = "h";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 8:
       num = "i";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 9:
       num = "j";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 10:
       num = "k";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 11:
       num = "l";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 12:
       num = "m";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 13:
       num = "n";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 14:
       num = "o";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 15:
       num = "p";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 16:
       num = "q";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 17:
       num = "r";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 18:
       num = "s";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 19:
       num = "t";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 20:
       num = "u";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 21:
       num = "v";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 22:
       num = "w";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 23:
       num = "x";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 24:
       num = "y";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
 
     case 25:
       num = "z";
       console.log(`num ${num}`);
+      link5 = link3 + num;
+      fetch(link5, requestOptions3)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
 
       break;
   }
