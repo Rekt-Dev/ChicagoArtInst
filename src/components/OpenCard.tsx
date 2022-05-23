8382d04375ba7d1b315de9f1c1c1e4c1521f001c
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-23 21:44:04 +0300

    openCard now opens the pic fullscreen soon it will render per our needs.

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -4,7 +4,10 @@ export function OpenCard() {
   function getImageData() {
     console.log("getImageData invoked thru openCard");
   }
+  const url = `https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg`;
   console.log("card was clicked");
+  window.open(url, "_blank");
+
   getImageData();
 
   return (
