ff0d28a0d2b4722a4ba96efcad27d37864a639d6
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-23 21:52:52 +0300

    started working on router

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -1,4 +1,6 @@
 import mockLink from "./Card";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export function OpenCard() {
   function getImageData() {
@@ -7,6 +9,9 @@ export function OpenCard() {
   const url = `https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg`;
   console.log("card was clicked");
   window.open(url, "_blank");
+  <Link to="/about" target="_blank">
+    About
+  </Link>;
 
   getImageData();
 
