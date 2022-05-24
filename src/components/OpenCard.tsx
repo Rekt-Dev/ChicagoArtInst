b843bb1a9c4a8e75dab290625fe501dc21b6bd63
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-24 19:47:15 +0300

    openCard fixings

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 
 export function OpenCard() {
-  function getImageData() {
-    console.log("getImageData invoked thru openCard");
-  }
-
   const url1 = "https://theRealFileIllBuild";
   const url = `https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg`;
   console.log("card was clicked");
   window.open(url, "_blank");
-
-  getImageData();
 
   return (
     <div className="center">
