d61094646101f119a97c5fb01ba26f0ed3e5357f
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-24 17:03:03 +0300

    app.css liFont class

--- src/components/OpenCard.tsx
+++ src/components/OpenCard.tsx
@@ -3,15 +3,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export function OpenCard() {
-
   function getImageData() {
     console.log("getImageData invoked thru openCard");
   }
-  
+
   const url = `https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg`;
   console.log("card was clicked");
   window.open(url, "_blank");
-  
 
   getImageData();
 
@@ -19,16 +17,14 @@ export function OpenCard() {
     <div className="center">
       <div>
         <ul>
-     
+          <li className="liFont">a peice of data loren ipsumish </li>
           <li>a peice of data loren ipsumish </li>
           <li>a peice of data loren ipsumish </li>
           <li>a peice of data loren ipsumish </li>
-          <li>a peice of data loren ipsumish </li>
-
         </ul>
-      <div>
-        <img width="1350" height="750" src={url} alt="a beautiful peice" />
-      </div>
+        <div>
+          <img width="1350" height="750" src={url} alt="a beautiful peice" />
+        </div>
       </div>
     </div>
   );
