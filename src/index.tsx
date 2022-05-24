f50ae892abb7b1852c51f351bc1878e1b96e59b7
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-24 20:23:17 +0300

    started messing with       <BrowserRouter>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -8,6 +8,8 @@ const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
