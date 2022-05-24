2d30c18e7c2e601611842b4ead0f461b53c23f0b
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-24 21:00:20 +0300

    Row component cre8d & applied

--- src/App.tsx
+++ src/App.tsx
@@ -5,72 +5,21 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import { CardDisplayer } from "./services/CardDisplayer";
 import { Link } from "react-router-dom";
+import { Row } from "./components/Row";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <div className="center" onClick={CardDisplayer}>
-        {json.map((obj) => (
-          <Card
-            key={obj.id}
-            artistName={obj.name}
-            title={obj.title}
-            year={obj.year}
-          />
-        ))}
-      </div>
-      <div>
-        <nav
-          style={{
-            paddingBottom: "1rem"
-          }}
-        >
-          <Link to="/i">Inloices</Link> | <Link to="/expenses">Expenses</Link>
-        </nav>
-      </div>
-      <div className="center" onClick={CardDisplayer}>
-        {json.map((obj) => (
-          <Card
-            key={obj.id}
-            artistName={obj.name}
-            title={obj.title}
-            year={obj.year}
-          />
-        ))}
-      </div>
-      <div className="center" onClick={CardDisplayer}>
-        {json.map((obj) => (
-          <Card
-            key={obj.id}
-            artistName={obj.name}
-            title={obj.title}
-            year={obj.year}
-          />
-        ))}
-      </div>
-      <div className="center" onClick={CardDisplayer}>
-        {json.map((obj) => (
-          <Card
-            key={obj.id}
-            artistName={obj.name}
-            title={obj.title}
-            year={obj.year}
-          />
-        ))}
-      </div>
-      <div className="center" onClick={CardDisplayer}>
-        {json.map((obj) => (
-          <Card
-            key={obj.id}
-            artistName={obj.name}
-            title={obj.title}
-            year={obj.year}
-          />
-        ))}
-      </div>
-      <br></br>
-      <br></br>) )
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
     </div>
   );
 }
---
+++ src/components/Row.tsx
@@ -0,0 +1,20 @@
+import { CardDisplayer } from "../services/CardDisplayer";
+import { json } from "../services/importedData";
+import Card from "../components/Card";
+
+export function Row() {
+  console.log(`row was activ8d`);
+
+  return (
+    <div className="center" onClick={CardDisplayer}>
+      {json.map((obj) => (
+        <Card
+          key={obj.id}
+          artistName={obj.name}
+          title={obj.title}
+          year={obj.year}
+        />
+      ))}
+    </div>
+  );
+}
