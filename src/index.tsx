44812b40a3bde6ebe4ae8ee38a8dde912833f020
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-25 12:09:13 +0300

    toying with homepage rendering adding componenets to check behavior

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { TestComponent } from "./components/TestComponent";
-
+import { Row } from "./components/Row";
+import { OpenCard } from "./components/OpenCard";
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
@@ -11,7 +12,9 @@ root.render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <OpenCard />
       <TestComponent />
+      <Row />
     </BrowserRouter>
   </StrictMode>
 );
