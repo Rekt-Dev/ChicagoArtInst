3cda6d0f283d69a0f29251ccacac169e76abfbe2
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-28 11:24:27 +0300

    cleaning up some console logs

--- src/services/feelingLucky.ts
+++ src/services/feelingLucky.ts
@@ -1,13 +1,8 @@
 import { requestOptions } from "./requestOptions";
 
 export function lucky() {
-  console.log(`feeling lucky pressed`);
-
   let value = Math.floor(Math.random() * 26);
   let r = (Math.random() * 10).toString(36).substring(9);
-
-  console.log("randomized", r);
-  console.log(`this is value ${value}`);
 
   const link1 = "https://api.artic.edu/api/v1/artworks/search?limit=1q=";
 
