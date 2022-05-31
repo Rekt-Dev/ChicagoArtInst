8ac99d313ecf2c134e61ee5dc52d8e6fc4b1c592
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-31 21:49:32 +0300

    text shorteming for gui responsiveness issue

--- src/services/importedData.tsx
+++ src/services/importedData.tsx
@@ -1,7 +1,7 @@
 export const json = [
   { name: "John", title: "title", year: "1971", id: "1" },
   { name: "second", title: "a title", year: "1745", id: "2" },
-  { name: "third", title: "best artwork ever", year: "1883", id: "3" },
+  { name: "third", title: "best artwork ", year: "1883", id: "3" },
   { name: "fourth", title: "a blue moon", year: "1434", id: "4" },
   { name: "fifth", title: "great title", year: "1929", id: "5" },
   { name: "fifth", title: "great title", year: "1929", id: "6" }
