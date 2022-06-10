0b09703651dc414b629c995fcd8440a10affbd37
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-06-10 21:24:35 +0300

    toying with router messing up app lotsa garbage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import "./styles.css";
 import "./index.css";
 import Header from "./components/Header";
-import { Link } from "react-router-dom";
+import {
+  Link,
+  BrowserRouter,
+  Routes,
+  Route,
+  Layout,
+  Home
+} from "react-router-dom";
 import { Card } from "./components/Card";
 import { json } from "./services/json";
-import {OpenCard} from "./components/OpenCard"
+import { OpenCard } from "./components/OpenCard";
 export default function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<OpenCard />}>
+          <Route index element={<Card />} />
+          <Route path="blogs" element={<OpenCard />} />
+          <Route path="contact" element={<OpenCard />} />
+          <Route path="*" element={<OpenCard />} />
+        </Route>
+      </Routes>
       <div>
-      <Link to="/JOSEPH.TLT">pages : 2-10 | </Link>
-      <Link to="/11_20">pages : 11-20 | </Link>
-      <Link to="/21_30">pages : 21-30 | </Link>
-      <Link to="/31_40">pages : 31-40 | </Link>
-    </div>
+        <Link to="OpenCard.tsx">OpenCard | </Link>
+        <Link to="/11_20">pages : 11-20 | </Link>
+        <Link to="/21_30">pages : 21-30 | </Link>
+        <Link to="/31_40">pages : 31-40 | </Link>
+      </div>
       <div className="centered">
         {json.map((obj) => (
           <div className="">
@@ -31,8 +46,6 @@ export default function App() {
       <Link to="/11_20">pages : 11-20 | </Link>
       <Link to="/21_30">pages : 21-30 | </Link>
       <Link to="/31_40">pages : 31-40 | </Link>
-      <OpenCard />
-</div>
-    
+    </div>
   );
 }
